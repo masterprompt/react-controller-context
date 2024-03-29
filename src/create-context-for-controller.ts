1ccf9900a35f885f561a1eba2a3e779e4fc2d1bd
Casey Kerr <masterprompt@gmail.com>
2024-03-29 11:31:38 -0500

    debugging

--- src/create-context-for-controller.ts
+++ src/create-context-for-controller.ts
@@ -6,6 +6,7 @@ export function createContextForController<TController> (
     useController: Controller<TController>
 ) {
     const context = React.createContext<TController>({} as TController)
+    console.log('createContextForController:', {context, useController})
     return {
         Provider: createControllerProvider(context, useController),
         useController: () => React.useContext(context)
